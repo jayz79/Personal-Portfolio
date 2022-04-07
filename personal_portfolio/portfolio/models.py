e5fbb707dcc719583cb6af65c50620a8d47ae766
jayz79 <jafethzuniga@gmail.com>
2022-04-07 07:26:12 -0500

    added bootstrap template examples to improve visuals - added blog functionality

--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -8,5 +8,7 @@ class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.title
     
     
