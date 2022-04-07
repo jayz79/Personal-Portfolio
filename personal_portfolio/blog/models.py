5500b729f88db714240c2e004940a1d8eb45c777
jayz79 <jafethzuniga@gmail.com>
2022-04-07 08:24:08 -0500

    revert adding image to blogs

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -5,7 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
-    image = models.ImageField(upload_to='portfolio/images')
 
     def __str__(self):
         return self.title
