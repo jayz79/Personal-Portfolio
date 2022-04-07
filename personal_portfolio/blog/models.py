4fdfdf8ff5e3a115559bbfe595ce2ee1bd70df78
jayz79 <jafethzuniga@gmail.com>
2022-04-07 08:23:20 -0500

    added css stylesheet from tutorial - image to blog posts

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
+    image = models.ImageField(upload_to='portfolio/images')
 
     def __str__(self):
         return self.title
