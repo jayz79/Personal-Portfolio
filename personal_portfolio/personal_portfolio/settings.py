1ef864b09e8b6550a288baa351cbec44f9969101
jayz79 <jafethzuniga@gmail.com>
2022-04-21 22:40:56 -0500

    updated SK visibility

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uxiu2&r&m)58+z8*=o%t72o(fm)=%ft0@_ot9t3razl0#_jst%'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
