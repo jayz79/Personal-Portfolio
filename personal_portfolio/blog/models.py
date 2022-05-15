884030f4a23a178420cdcd345bef61dff7d4915a
jayz79 <jafethzuniga@gmail.com>
2022-05-14 22:57:22 -0500

    made videos database

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Model for Blog component
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -8,3 +8,11 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+# Model for uploading videos component
+class Video(models.Model):
+    video_name = models.CharField(max_length=250) # limit video name to 250 chars
+    video_file = models.FileField(upload_to='videos/', null=True, verbose_name="")
+
+    def __str__(self):
+        return self.name + ": " + str(self.video_file)
