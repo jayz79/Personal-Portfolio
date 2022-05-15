cd2e58cbaee2cb0d2e51169b54df7dea32974421
jayz79 <jafethzuniga@gmail.com>
2022-05-14 23:18:55 -0500

    fixed import error for video model, started show video view

--- personal_portfolio/blog/forms.py
+++ personal_portfolio/blog/forms.py
@@ -1,7 +1,8 @@
 # ModelForm file, used for the video database
-
+from django import forms
+from .models import Video
 class VideoForm(forms.ModelForm):
 
     class MetaData:
-        model = Video
+        model = Video.objects
         fields = ["name", "videofile"]
--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Blog
+from .models import Blog, Video
+from .forms import VideoForm
 
 
-# Create your views here.
+# Returns all blogs from db
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
 
+# Returns info of desired blog with its blog_id
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html',{'blog':blog})
+
+# Returns most recently uploaded video 
+def show_video(request):
+    latest_video = Video.objects.last()
+    video_file = latest_video.video_file
