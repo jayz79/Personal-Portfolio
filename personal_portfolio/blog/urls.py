57064ee00d0d756604d5333b982ddd17d36a4a1b
jayz79 <jafethzuniga@gmail.com>
2022-05-14 22:52:24 -0500

    static media add to urls file

--- personal_portfolio/blog/urls.py
+++ personal_portfolio/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.conf import *
 
 app_name = 'blog'
 
@@ -7,5 +8,5 @@ urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     path('<int:blog_id>/', views.detail, name='detail'),
 
-]
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
